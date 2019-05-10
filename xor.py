ad9d45a5842944353fd814cd4d62f72ff67fd191
Defelo <elodef42@gmail.com>
2019-05-10 23:42:59 +0200

    Add evaluator

--- xor.py
+++ xor.py
@@ -1,30 +1,21 @@
 from typing import List
 
+from evaluator import Evaluator
 from genome import Genome
 from pool import Pool
 
-
-def evaluate(genome: Genome) -> float:
-    fitness: float = 0
-    for i in range(2):
-        for j in range(2):
-            fitness += 1 - abs((i ^ j) - genome.evaluate_network([i, j])[0])
-    return fitness ** 2
-
-
+evaluator: Evaluator = Evaluator([([i, j], [i ^ j]) for i in range(2) for j in range(2)])
 pool: Pool = Pool()
 generation: int = 1
 while True:
-    pool.evaluate_fitness(evaluate)
+    pool.evaluate_fitness(evaluator.evaluate)
     top_genome: Genome = pool.get_top_genome()
     print(f"Generation {generation} | Top Fitness: {top_genome.fitness} | Species: {len(pool.species)}")
-    if top_genome.fitness > 15:
+    if top_genome.fitness > 3.99:
         break
     pool.breed_new_generation()
     generation += 1
 
-for i in range(2):
-    for j in range(2):
-        network_input: List[float] = [i, j]
-        network_output: List[float] = top_genome.evaluate_network(network_input)
-        print(network_input, "->", network_output)
+for network_input, _ in evaluator.test_cases:
+    network_output: List[float] = top_genome.evaluate_network(network_input)
+    print(network_input, "->", network_output)
