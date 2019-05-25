85f8d466d29d91740f8f803e55d8c7162ce73051
Defelo <elodef42@gmail.com>
2019-05-25 21:00:28 +0200

    cleanup for v2

--- innovationcounter.py
+++
@@ -1,7 +0,0 @@
-class InnovationCounter:
-    innovation: int = 0
-
-    @staticmethod
-    def new_innovation():
-        InnovationCounter.innovation += 1
-        return InnovationCounter.innovation
--- nodegene.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-from connectiongene import ConnectionGene
-
-
-class NodeGene:
-    def __init__(self, value: float):
-        self.value: float = value
-        self.incoming: List[ConnectionGene] = []
